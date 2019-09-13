72ca9a8661d6d4e94d09a2bb9eff7ab01d038caf
bgpat <bgpat@users.noreply.github.com>
2019-09-14 01:33:22 +0900

    Improve selecting performance

--- src/overlay/background.js
+++ src/overlay/background.js
@@ -12,10 +12,18 @@ export default class Background extends G {
   render(topElement, { left, top, right, bottom }) {
     this.path.d = Array.from(document.querySelectorAll('[id]'))
       .map(e => e.getBoundingClientRect())
-      .filter(bcr => left <= bcr.left || bcr.left <= right)
-      .filter(bcr => left <= bcr.right || bcr.right <= right)
-      .filter(bcr => top <= bcr.top || bcr.top <= bottom)
-      .filter(bcr => top <= bcr.bottom || bcr.bottom <= bottom)
+      .filter(
+        bcr =>
+          (left <= bcr.left && bcr.left <= right) ||
+          (left <= bcr.right && bcr.right <= right),
+      )
+      .filter(
+        bcr =>
+          (top <= bcr.top && bcr.top <= bottom) ||
+          (top <= bcr.bottom && bcr.bottom <= bottom),
+      )
+      .filter(bcr => bcr.left !== bcr.right)
+      .filter(bcr => bcr.top !== bcr.bottom)
       .map(bcr => [
         { x: bcr.left, y: bcr.top },
         { x: bcr.right, y: bcr.top },
